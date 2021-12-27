6015c6de0e042089c1c47735c5bfd1f2a214f3eb
Kevin Lima <kevinlima138@gmail.com>
2021-12-27 15:13:06 -0300

    Atualizando Arquivos

--- Modulo_2/aula1.js
+++ Modulo_2/aula1.js
@@ -1,8 +1,46 @@
 // var
 
-var altura = 5;
-var comprimento = 6;
+// var altura = 5;
+// var comprimento = 6;
 
-var area = altura * comprimento;
+// var area = altura * comprimento;
 
-console.log(area)
+// console.log(area);
+
+// let
+
+// let forma = "retangulo";
+// let altura = 5;
+// let comprimento = 7;
+
+// if (forma === "retangulo")
+// {
+//     area = altura * comprimento;
+// }
+// else
+// {
+//     area = (altura * comprimento) / 2;
+// }
+
+// console.log(area);
+// let area;
+
+//const = precisa sempre ter um valor para a variavel
+
+
+const forma = 'quadrado';
+const altura = 5;
+const comprimento = 7;
+let area;
+
+if(forma === 'quadrado')
+{
+    area = altura * comprimento;
+}
+else 
+{
+    area = (altura * comprimento) / 2;
+}
+
+
+console.log(area);
