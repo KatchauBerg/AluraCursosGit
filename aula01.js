66ff5f1398fc717361a14619c6907590688195ed
Kevin Lima <kevinlima138@gmail.com>
2021-12-27 14:05:10 -0300

    subindo arquivos curso de javascript

---
+++ aula01.js
@@ -0,0 +1,20 @@
+//Tipos de Variaveis
+
+const meuNumero = 3;
+ 
+const num1 = 1;
+const num2 = 2;
+
+const soma = num1 * num2;
+
+console.log(soma);
+
+
+//ponto flutuante
+
+const numFlutuante = 3.3;
+const numFlutuante2 = .5; //recebe 0.5
+
+const sub = numFlutuante - numFlutuante2;
+
+console.log(sub);
---
+++ aula02.js
@@ -0,0 +1,18 @@
+//Aula de Strings
+
+const text1 = "Olá Mundo";
+const nome = 'Kevin De Souza Lima';
+const stringDeNumeros = "12345346567";
+
+
+const citacao = 'Meu Nome É:';
+
+console.log(text1);
+console.log(stringDeNumeros);
+console.log(citacao);
+
+//concatenação (+)
+
+console.log(citacao + nome);
+
+
