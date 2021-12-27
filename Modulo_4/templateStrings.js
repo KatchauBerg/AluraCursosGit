6c061e67b39ccb4ade51df2c35e7cf59c81d4344
Kevin Lima <kevinlima138@gmail.com>
2021-12-27 17:04:18 -0300

    Atualizando Arquivos

---
+++ Modulo_4/templateStrings.js
@@ -0,0 +1,8 @@
+const nome = "Kevin"
+const idade = 20
+const cidade = "Marília"
+
+
+const apresentacao = `meu nome é ${nome}, minha idade é ${idade}, e nasci em ${cidade}`;
+
+console.log(apresentacao);
---
+++ Modulo_4/ternario.js
@@ -0,0 +1,14 @@
+const idadeMinima = 18;
+let idadeClient = 19;
+
+// if (idadeClient >= idadeMinima)
+// {
+//     console.log("Cerveja");
+// }
+// else
+// {
+// //     console.log("Suco")
+// }
+
+
+console.log(idadeClient >= idadeMinima ? "Cerveja" : "Suco")
